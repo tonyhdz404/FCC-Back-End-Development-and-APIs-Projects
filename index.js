792cc4b6167e49400de063f1894aa6aa6129f6e6
tonyhdz404 <antonio.hernandez030@gmail.com>
2022-07-28 09:20:58 -0600

    Error

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ app.get("/api/:date?", (req, res) => {
 });
 
 // listen for requests :)
-// var listener = app.listen(process.env.PORT, function () {
-//   console.log("Your app is listening on port " + listener.address().port);
-// });
-var listener = app.listen(3000, function () {
-  console.log("Your app is listening on port " + 3000);
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
+// var listener = app.listen(3000, function () {
+//   console.log("Your app is listening on port " + 3000);
+// });
